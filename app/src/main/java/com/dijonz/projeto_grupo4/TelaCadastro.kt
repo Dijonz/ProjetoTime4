50350b0cf59f6493bbafea7b4e50f6b3f0c30ba1
joao <jpcarvalhof@outlook.com>
2023-04-03 23:26:46 -0300

    Alterações de usabilidade e formatação do código

--- app/src/main/java/com/dijonz/projeto_grupo4/TelaCadastro.kt
+++ app/src/main/java/com/dijonz/projeto_grupo4/TelaCadastro.kt
@@ -3,9 +3,7 @@ package com.dijonz.projeto_grupo4
 import android.graphics.Color
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dijonz.projeto_grupo4.databinding.TelaCadastroBinding
@@ -18,56 +16,46 @@ class MainActivity : AppCompatActivity() {
     private val db = Firebase.firestore
     private lateinit var binding: TelaCadastroBinding
 
-    private fun verificaVazio(nome: EditText, numero: EditText, email: EditText, senha: EditText, endereco: EditText, curriculo: EditText): Boolean{
-        if(TextUtils.isEmpty(nome.text) || TextUtils.isEmpty(numero.text) || TextUtils.isEmpty(email.text) || TextUtils.isEmpty(senha.text) || TextUtils.isEmpty(endereco.text) || TextUtils.isEmpty(curriculo.text)) {
-            return false }
-        else{
-            return true
-        }
-    }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = TelaCadastroBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
         binding.btCadastrar.setOnClickListener {
 
+            if(emptyVerifier(binding.etNomeCompleto, binding.etTelefone, binding.etEmail, binding.etSenha, binding.etEndereco, binding.etMiniCurriculo)) {
 
-            if(verificaVazio(binding.etNomeCompleto, binding.etTelefone, binding.etEmail, binding.etSenha, binding.etEndereco, binding.etMiniCurriculo)) {
+                    val dentista = hashMapOf(
+                        "nome" to binding.etNomeCompleto.text.toString(),
+                        "telefone" to binding.etTelefone.text.toString().toInt(),
+                        "endereco1" to binding.etEndereco.text.toString(),
+                        "email" to binding.etEmail.text.toString(),
+                        "senha" to binding.etSenha.text.toString(),
+                        "curriculo" to binding.etMiniCurriculo.text.toString()
+                    )
 
-
-                val dentista = hashMapOf(
-                    "nome" to binding.etNomeCompleto.text.toString(),
-                    "telefone" to binding.etTelefone.text.toString().toInt(),
-                    "endereco1" to binding.etEndereco.text.toString(),
-                    "email" to binding.etEmail.text.toString(),
-                    "senha" to binding.etSenha.text.toString(),
-                    "curriculo" to binding.etMiniCurriculo.text.toString()
-                )
-
-                db.collection("dentistas")
-                    .add(dentista)
-                    .addOnSuccessListener {
-                        Toast.makeText(this, "Cadastro Realizado", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this, "ERRO", Toast.LENGTH_SHORT).show()
-                    }
-            } else {
-
-                val snack = Snackbar.make(findViewById(android.R.id.content), "Olá, Snackbar!", Snackbar.LENGTH_SHORT)
+                    db.collection("dentistas")
+                        .add(dentista)
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "Cadastro Realizado", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, "ERRO", Toast.LENGTH_SHORT).show()
+                        }
+            }
+            else{
+                val snack = Snackbar.make((binding.root),"Preencha todos os dados!",Snackbar.LENGTH_SHORT)
                 snack.setBackgroundTint(Color.RED)
                 snack.setTextColor(Color.WHITE)
                 snack.show()
-
-
-
-
             }
         }
+    }
+
+
+    private fun emptyVerifier(nome: EditText, numero: EditText, email: EditText, senha: EditText, endereco: EditText, curriculo: EditText): Boolean{
+        return !(TextUtils.isEmpty(nome.text) || TextUtils.isEmpty(numero.text) ||TextUtils.isEmpty(email.text) ||
+                TextUtils.isEmpty(senha.text) || TextUtils.isEmpty(endereco.text) || TextUtils.isEmpty(curriculo.text))
     }
 }
