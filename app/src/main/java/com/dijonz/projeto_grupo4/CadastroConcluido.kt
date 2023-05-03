a2cdf33f23bee1a8aac6e2979b65575de0237e1f
joao <jpcarvalhof@outlook.com>
2023-05-02 23:30:38 -0300

    Firebase Cloud Messaging

--- app/src/main/java/com/dijonz/projeto_grupo4/CadastroConcluido.kt
+++ app/src/main/java/com/dijonz/projeto_grupo4/CadastroConcluido.kt
@@ -1,14 +1,21 @@
 package com.dijonz.projeto_grupo4
 
+import android.app.PendingIntent
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.edit
 import com.dijonz.projeto_grupo4.databinding.ActivityCadastroConcluidoBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class CadastroConcluido : AppCompatActivity() {
     private lateinit var binding: ActivityCadastroConcluidoBinding
@@ -41,6 +48,33 @@ class CadastroConcluido : AppCompatActivity() {
                 }
             }
         }
+
+    class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+        override fun onMessageReceived(remoteMessage: RemoteMessage) {
+            // Verifique se a mensagem contém dados
+            if (remoteMessage.data.isNotEmpty()) {
+                val data = remoteMessage.data
+
+                val title = data["title"]
+                val body = data["body"]
+                val documentId = data["documentId"]
+
+                val intent = Intent(this, CadastroConcluido::class.java)
+                intent.putExtra("documentId", documentId)
+                val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+                val notificationBuilder = NotificationCompat.Builder(this, "channelId")
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+
+                val notificationManager = NotificationManagerCompat.from(this)
+                notificationManager.notify(0, notificationBuilder.build())
+            }
+        }
+    }
 
 
 
@@ -88,6 +122,5 @@ class CadastroConcluido : AppCompatActivity() {
             }
         return id
     }
-
 }
 
