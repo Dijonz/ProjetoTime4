3bc5020213566775bf0cabd8fa49ca0ba9692f6c
joao <jpcarvalhof@outlook.com>
2023-06-13 23:47:28 -0300

    TelaEmergenciaACEITA-TESTES

--- app/src/main/java/com/dijonz/projeto_grupo4/EmergenciaAceita.kt
+++ app/src/main/java/com/dijonz/projeto_grupo4/EmergenciaAceita.kt
@@ -8,7 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dijonz.projeto_grupo4.databinding.ActivityEmergenciaAceitaBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -22,74 +24,86 @@ class EmergenciaAceita : AppCompatActivity() {
     private var auth = FirebaseAuth.getInstance()
     private var db = Firebase.firestore
     private lateinit var binding: ActivityEmergenciaAceitaBinding
-    private var userlat = LatLng(12.3,12.3)
+    private var userlat: LatLng = LatLng(0.00, 0.00)
+    private var fusedLocationProviderClient: FusedLocationProviderClient? = null
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEmergenciaAceitaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val uidsocorro= intent.getStringExtra("uid-socorrista")
+        /*requestLocationPermission()
+        checkLocationPermission()*/
 
-        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return exitProcess(0)
-        }
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                val currentlatitude = location!!.latitude
-                val currentlongitude = location!!.longitude
-                val currentLatLng = LatLng(currentlatitude,currentlongitude)
 
-                db.collection("emergencias")
-                    .document(uidsocorro.toString())
-                    .get()
-                    .addOnSuccessListener { result ->
-                        val latuser: Double = result.data?.get("latitude") as Double
-                        val longuser: Double = result.data?.get("longitude") as Double
-                        userlat = LatLng(latuser,longuser)
-                    }
-
-                val mapFragment =
-                    supportFragmentManager.findFragmentById(R.id.cvLocalizacao) as SupportMapFragment
-                mapFragment.getMapAsync{ googleMap ->
-
-                    googleMap?.run{
-                        drawRouteOnMap(
-                            getString(R.string.google_api_key),
-                            source = currentLatLng,
-                            destination = userlat,
-                            context = applicationContext
-                        )
-                    }
-                }
-            }
+        val uidsocorro = intent.getStringExtra("uid-socorrista")
 
         val uidSocorrista = intent.getStringExtra("uid-socorrista")
-
-
-        db.collection("emergencias").document(uidSocorrista.toString()).get().addOnSuccessListener {result ->
-            binding.tvNome.text = result.data?.get("nome")?.toString()
-            binding.tvTelefone.text = result.data?.get("telefone")?.toString()
-        }
+        db.collection("emergencias").document(uidSocorrista.toString()).get()
+            .addOnSuccessListener { result ->
+                binding.tvNome.text = result.data?.get("nome")?.toString()
+                binding.tvTelefone.text = result.data?.get("telefone")?.toString()
+            }
 
 
         binding.cvEmergencias.setOnClickListener {
-            db.collection("emergencias").document(uidSocorrista.toString()).update("status", "finalizado").addOnSuccessListener {
-                Toast.makeText(this,"EMERGÊNCIA ENCERRADA",Toast.LENGTH_LONG)
-                Thread.sleep(2_000)
-                val intent = Intent(this,CadastroConcluido::class.java)
-                startActivity(intent)
+            db.collection("emergencias").document(uidSocorrista.toString())
+                .update("status", "finalizado").addOnSuccessListener {
+                    Toast.makeText(this, "EMERGÊNCIA ENCERRADA", Toast.LENGTH_LONG).show()
+                    Thread.sleep(2_000)
+                    val intent = Intent(this, CadastroConcluido::class.java)
+                    startActivity(intent)
+                }
+        }
+
+        /*if (checkLocationPermission()){
+        db.collection("emergencias")
+            .document(uidsocorro.toString())
+            .get()
+            .addOnSuccessListener { result ->
+                val latuser= result.data?.get("latitude") as Double
+                val longuser = result.data?.get("longitude") as Double
+                userlat = LatLng(latuser,longuser)
+
+            fusedLocationProviderClient!!.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        val currentlatitude = location!!.latitude
+                        val currentlongitude = location!!.longitude
+                        val currentLatLng = LatLng(currentlatitude, currentlongitude)*/
+
+                        val mapFragment =
+                            supportFragmentManager.findFragmentById(R.id.cvLocalizacao) as SupportMapFragment
+                        mapFragment.getMapAsync { googleMap ->
+
+                            googleMap?.run {
+                                drawRouteOnMap(
+                                    getString(R.string.google_api_key),
+                                    source = LatLng(-23.587157,-46.6825137),
+                                    destination = LatLng(-23.5870167,-46.6690508),
+                                    context = applicationContext
+                                )
+                            }
+                        }
+                    }
             }
+        /*}else{
+            requestLocationPermission()
         }
 
     }
+
+    private fun requestLocationPermission() {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        ActivityCompat.requestPermissions(this, arrayOf(permission), LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        val result = ContextCompat.checkSelfPermission(this, permission)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
 }
+*/
